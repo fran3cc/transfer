a7c6d3241e8943f47422382a5d299fe640ff82e2
chen sihan <chensihan@chendeMacBook-Pro-6.local>
2024-01-11 00:41:46 +0800

    s

--- code/B/model.py
+++ code/B/model.py
@@ -1,5 +1,3 @@
-# B/model.py
-
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -9,10 +7,13 @@ class NiNBlock(nn.Module):
         super(NiNBlock, self).__init__()
         self.block = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(out_channels, out_channels, kernel_size=1),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
             nn.Conv2d(out_channels, out_channels, kernel_size=1),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
         )
 
@@ -22,17 +23,25 @@ class NiNBlock(nn.Module):
 class NiN(nn.Module):
     def __init__(self, num_classes=9):
         super(NiN, self).__init__()
-        self.nin_block1 = NiNBlock(3, 32, kernel_size=5, stride=1, padding=2)
-        self.nin_block2 = NiNBlock(32, 64, kernel_size=5, stride=1, padding=2)
-        self.nin_block3 = NiNBlock(64, 128, kernel_size=3, stride=1, padding=1)
+        self.nin_block1 = NiNBlock(3, 48, kernel_size=5, stride=1, padding=2)
+        self.nin_block2 = NiNBlock(48, 96, kernel_size=5, stride=1, padding=2)
+        self.nin_block3 = NiNBlock(96, 192, kernel_size=3, stride=1, padding=1)
+        self.nin_block4 = NiNBlock(192, 192, kernel_size=3, stride=1, padding=1)  # New block
         self.dropout = nn.Dropout(0.5)
-        self.final_block = NiNBlock(128, num_classes, kernel_size=3, stride=1, padding=1)
+        self.final_block = NiNBlock(192, num_classes, kernel_size=3, stride=1, padding=1)
         self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
 
+        # Residual connections
+        self.residual1 = nn.Conv2d(3, 48, kernel_size=1, stride=1, padding=0)
+        self.residual2 = nn.Conv2d(48, 96, kernel_size=1, stride=1, padding=0)
+        self.residual3 = nn.Conv2d(96, 192, kernel_size=1, stride=1, padding=0)
+        self.residual4 = nn.Conv2d(192, 192, kernel_size=1, stride=1, padding=0)
+
     def forward(self, x):
-        x = self.nin_block1(x)
-        x = self.nin_block2(x)
-        x = self.nin_block3(x)
+        x = self.nin_block1(x) + self.residual1(x)
+        x = self.nin_block2(x) + self.residual2(x)
+        x = self.nin_block3(x) + self.residual3(x)
+        x = self.nin_block4(x) + self.residual4(x)
         x = self.dropout(x)
         x = self.final_block(x)
         x = self.global_avg_pool(x)

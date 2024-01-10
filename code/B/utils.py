b4ae9600a331e9c6db4ef86122870433b601b43a
chen sihan <chensihan@chendeMacBook-Pro-6.local>
2024-01-11 02:18:52 +0800

    s

--- code/B/utils.py
+++ code/B/utils.py
@@ -5,6 +5,8 @@ import torch
 from torchvision import transforms
 from torch.utils.data import DataLoader, Dataset
 
+from PIL import Image
+
 class PathMNISTDataset(Dataset):
     def __init__(self, images, labels, transform=None):
         self.images = images
@@ -17,9 +19,15 @@ class PathMNISTDataset(Dataset):
     def __getitem__(self, idx):
         image = self.images[idx]
         label = self.labels[idx]
+
+        # 将 NumPy 数组转换为 PIL 图像
+        image = Image.fromarray(image)
+
         if self.transform:
             image = self.transform(image)
+
         return image, label
+
 
 def load_data(data_path, batch_size=32):
     data = np.load(data_path)

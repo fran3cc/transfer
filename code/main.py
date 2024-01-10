6c7123588dc666d4d6d010380ece85e18b0de780
chen sihan <chensihan@chendeMacBook-Pro-6.local>
2024-01-10 23:13:37 +0800

    s

--- code/main.py
+++ code/main.py
@@ -9,15 +9,19 @@
 
 from B.train import train_model
 from B.evaluate import evaluate_model
-import torch
 
 if __name__ == "__main__":
     data_path = "./Datasets/pathmnist.npz"
-    model = train_model(data_path, epochs=30, batch_size=32, lr=0.001)
+    num_classes = 9  # Set this to the number of classes in your dataset
+    epochs = 30
+    batch_size = 32
+    learning_rate = 0.001
+
+    model = train_model(data_path, num_classes, epochs, batch_size, learning_rate)
 
     # Saving the model
     torch.save(model.state_dict(), './B/pathmnist_model.pth')
 
     # Load model for evaluation
     model.load_state_dict(torch.load('./B/pathmnist_model.pth'))
-    evaluate_model(model, data_path, batch_size=32)
+    evaluate_model(model, data_path, batch_size)
